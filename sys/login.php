eac59a8fdc0784860db2ce2bc48ed1c993ce9939
Izumi Yusuke <cosmosas11@gmail.com>
2019-06-18 16:35:11 +0900

    Yusuke.i 1635

--- sys/login.php
+++ sys/login.php
@@ -1,6 +1,23 @@
 <?php
 
+// 設定ファイルのパース
 $ini = parse_ini_file('../config/conf.ini');
+
+// POSTのID情報の有無の確認
+if(empty($_POST["l_id"])){
+    header("location: ../index.php?err=id_b");
+    exit();
+}
+
+// POSTのPASSWORD情報の確認
+if(empty($_POST["l_ps"])){
+    header("location: ../index.php?err=ps_b");
+    exit();
+}
+
+
+// ハッシュで暗号化
+$ps2 = sha1($_POST["l_ps"]);
 
 
 
