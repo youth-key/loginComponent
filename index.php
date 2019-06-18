9e638762ac5d5f0a558e9be7517aa0f2a68d4d21
Izumi Yusuke <cosmosas11@gmail.com>
2019-06-18 16:20:59 +0900

    Yusuke.i

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>Please Id & Password.</h1>
+    <form action="sys/login.php" method="post">
+        <span>ID<input type="text" name="l_id"></span>
+        <span>PASSWORD<input type="password" name="l_ps"></span>
+        <input type="submit" value="OK">
+    </form>
+    
+</body>
+
+</html>
